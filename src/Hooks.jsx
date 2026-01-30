dabaa2e06d56cee8a4535d297fbe9c412110ffa6
Cyrine Ben Hlila <syrinebh05@gmail.com>
2026-01-30 13:45:04 +0100

    Add useState and useEffect to Hooks component
    Imported useState and useEffect from React and integrated them into the Hooks component. Added state management for 'count' and two useEffect hooks to demonstrate side effects on mount and when 'count' changes.

--- src/Hooks.jsx
+++ src/Hooks.jsx
@@ -1,10 +1,28 @@
-import react from 'react';
-
+import react, { useState, useEffect } from 'react';
+    
+//function Hooks({name,description,link}) {
 function Hooks(props) {
-    //function Hooks({name,description,link}) {
+    
     //exemple de props par la méthode de destructuration
     const { name, description, link } = props;
+   
+    //consiste à extraire des valeurs d'un objet ou d'un tableau et à les assigner à des variables distinctes
+    //useState : permet de gérer l'état local dans un composant fonctionnel
     const [count, setCount] = react.useState(8);
+    
+    //useEffect : permet de gérer les effets de bord dans un composant fonctionnel
+    //Dans la phase de mise à jour : Updating
+    useEffect(() => {
+        console.log("Component mounted or updated");
+    }, [count]); //le tableau de dépendance
+    //Dans la phase de montage et de mise à jour : Mounting   
+    useEffect(() => {
+        console.log("Component mounted or updated");
+    }, []); //le tableau de dépendance
+    /*maghir mathot el tableau ray bch tsir el rendering kol mara ybddl el *état
+    wkteli ma mathot hata 7aja f el tableau ybda yetsir el rendering marra wa7da bark f el montage uniquement
+    wkteli mathot variable fel tableau ybda yetsir el rendering kol mara tbddl el variable hadi
+    */
     return (
         <><div>
             <h1>Hello {name}</h1>
